bcc01275c54f778b3537d5ed132246d883688418
Fuad Mustamirrul Ishlah <fuad.mustamirrul@citra.digital>
2020-01-11 13:06:23 +0700

    add string wrapper and documentation (v1.1)

--- cross.go
+++ cross.go
@@ -1,13 +1,68 @@
 package cross
 
+import (
+	"errors"
+	"strconv"
+	"strings"
+)
+
+//CrosspointCountString is a string wrapper for CrosspointCount
+//param:
+//	s => input string to be parsed to the necessary CrosspointCount params
+//		 format:
+//			- number separated with a space " "
+//			- X Axis and Y Axis is separated by a dash "-"
+//			- example = "2 4 1 2 5 3-3 2 3 1"
+func CrosspointCountString(s string) (int, error) {
+	splitted := strings.Split(s, "-")
+	if len(splitted) != 2 {
+		return -1, errors.New("invalid-input")
+	}
+
+	xAxis, err := convertStringToIntSlice(splitted[0], " ")
+	if err != nil {
+		return -1, err
+	}
+	yAxis, err := convertStringToIntSlice(splitted[1], " ")
+	if err != nil {
+		return -1, err
+	}
+	return CrosspointCount(xAxis, yAxis), nil
+}
+
+func convertStringToIntSlice(s, separator string) ([]int, error) {
+	numberStringSlice := strings.Split(s, separator)
+	intSlice := make([]int, 0)
+	for _, numberString := range numberStringSlice {
+		intVal, err := strconv.Atoi(numberString)
+		if err != nil {
+			return nil, err
+		}
+		intSlice = append(intSlice, intVal)
+	}
+	return intSlice, nil
+}
+
+//CrosspointCount counts the number of crosspoints of vertical and horizontal lines
+//params:
+//	xAxis is an int array, each element specifies the length of vertical lines in (index+1) position on x Axis
+//	yAxis is an int array, each element specifies the length of horizontal lines in (index+1) position on y Axis
 func CrosspointCount(xAxis, yAxis []int) int {
+	//Counter var
 	crosspointCount := 0
+	//for each line in y Axis
 	for yLinePos, yLineLength := range yAxis {
+		//get the number of times we need to check for crosspoint for this line
+		//if the number of lines in x Axis is smaller than the length of current Y axis line
+		//then we don't need to check the overflowed lines
 		limit := yLineLength
 		if len(xAxis) < yLineLength {
 			limit = len(xAxis)
 		}
 		for i := 0; i < limit; i++ {
+			//if i'th position is not taller than the position of current y line
+			//then ignore it
+			//else that is a crosspoint
 			if xAxis[i] < yLinePos+1 {
 				continue
 			}
--- cross_test.go
+++ cross_test.go
@@ -6,17 +6,46 @@ import (
 
 func TestCrosspointCount(t *testing.T) {
 	BasicTestCrosspointCount(t, []int{2, 4, 1, 2, 5, 3}, []int{3, 2, 3, 1}, 6)
+	BasicTestCrosspointCount(t, []int{1, 1}, []int{1, 1}, 1)
+	BasicTestCrosspointCount(t, []int{1, 1}, []int{100}, 2)
+	BasicTestCrosspointCount(t, []int{2, 2, 2}, []int{10, 10, 10}, 6)
 }
 func BasicTestCrosspointCount(t *testing.T, xAxis, yAxis []int, expected int) {
 	if output := CrosspointCount(xAxis, yAxis); output != expected {
-		t.Errorf(
-			`Test Fail
+		t.Errorf(`
+			Test Fail
 			Input: 
 				xAxis = %v
 				yAxis = %v
 			Expected: %v
 			But got: %v`,
 			xAxis, yAxis, expected, output,
+		)
+	}
+}
+func TestCrosspointCountString(t *testing.T) {
+	BasicTestCrosspointCountString(t, "2 4 1 2 5 3-3 2 3 1", 6, false)
+	BasicTestCrosspointCountString(t, "2 4 1 2 5 3-a 2 3 1", 6, true)
+	BasicTestCrosspointCountString(t, "2 4 1 2 5 3 3 2 3 1", 6, true)
+	BasicTestCrosspointCountString(t, "2 4 1 2 5 3/3 2 3 1", 6, true)
+}
+func BasicTestCrosspointCountString(t *testing.T, input string, expected int, expectingErr bool) {
+	output, err := CrosspointCountString(input)
+	if expectingErr != (err != nil) {
+		t.Errorf(`
+			Test Fail
+			Expecting error = %v
+			But got error: %v`,
+			expectingErr, err,
+		)
+	}
+	if output != expected && !expectingErr {
+		t.Errorf(`
+			Test Fail
+			Input: %s
+			Expected: %v
+			But got: %v`,
+			input, expected, output,
 		)
 	}
 }
